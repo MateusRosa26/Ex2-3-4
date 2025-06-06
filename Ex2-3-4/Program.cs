d1e24851ae64910e091f51b96e0f53fe0290e29c
MateusRosa22 <mateus9898@gmail.com>
2025-06-06 14:14:30 -0300

    organização melhorada

--- Ex2-3-4/Program.cs
+++ Ex2-3-4/Program.cs
@@ -25,35 +25,4 @@ namespace VendaIngressos
             Console.WriteLine($"Ingressos disponíveis: {quantidadeDisponivel}");
         }
     }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Ingresso show1 = new Ingresso();
-            show1.nomeDoShow = "Rock in Rio";
-            show1.preco = 250.00;
-            show1.quantidadeDisponivel = 1500;
-
-            Console.WriteLine("=== INFORMAÇÕES INICIAIS ===");
-            show1.ExibirInformacoes();
-
-            Console.WriteLine("\n=== ALTERANDO PREÇO ===");
-            show1.AlterarPreco(280.00);
-            show1.ExibirInformacoes();
-
-            Console.WriteLine("\n=== ALTERANDO QUANTIDADE ===");
-            show1.AlterarQuantidade(1200);
-            show1.ExibirInformacoes();
-
-            Console.WriteLine("\n=== SEGUNDO SHOW ===");
-            Ingresso show2 = new Ingresso();
-            show2.nomeDoShow = "Festival de Jazz";
-            show2.preco = 180.50;
-            show2.quantidadeDisponivel = 800;
-            show2.ExibirInformacoes();
-
-            Console.ReadKey();
-        }
-    }
 }
