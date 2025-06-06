be05fa2cff0f683eadbba8c952fa24063cca2c18
MateusRosa22 <mateus9898@gmail.com>
2025-06-06 14:01:53 -0300

    Adicionar arquivos de projeto.

---
+++ Ex2-3-4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace VendaIngressos
+{
+    class Ingresso
+    {
+        public string nomeDoShow;
+        public double preco;
+        public int quantidadeDisponivel;
+
+        public void AlterarPreco(double novoPreco)
+        {
+            preco = novoPreco;
+        }
+
+        public void AlterarQuantidade(int novaQuantidade)
+        {
+            quantidadeDisponivel = novaQuantidade;
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Show: {nomeDoShow}");
+            Console.WriteLine($"Preço: R$ {preco:F2}");
+            Console.WriteLine($"Ingressos disponíveis: {quantidadeDisponivel}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ingresso show1 = new Ingresso();
+            show1.nomeDoShow = "Rock in Rio";
+            show1.preco = 250.00;
+            show1.quantidadeDisponivel = 1500;
+
+            Console.WriteLine("=== INFORMAÇÕES INICIAIS ===");
+            show1.ExibirInformacoes();
+
+            Console.WriteLine("\n=== ALTERANDO PREÇO ===");
+            show1.AlterarPreco(280.00);
+            show1.ExibirInformacoes();
+
+            Console.WriteLine("\n=== ALTERANDO QUANTIDADE ===");
+            show1.AlterarQuantidade(1200);
+            show1.ExibirInformacoes();
+
+            Console.WriteLine("\n=== SEGUNDO SHOW ===");
+            Ingresso show2 = new Ingresso();
+            show2.nomeDoShow = "Festival de Jazz";
+            show2.preco = 180.50;
+            show2.quantidadeDisponivel = 800;
+            show2.ExibirInformacoes();
+
+            Console.ReadKey();
+        }
+    }
+}
